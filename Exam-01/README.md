5dc0e7e692bac454f55186a7b745f56e9b1fe122
kdmitrievsky <95374196+kdmitrievsky@users.noreply.github.com>
2022-04-07 23:59:31 +0300

    Update Exam 01

--- Exam-01/README.md
+++ Exam-01/README.md
@@ -18,4 +18,26 @@ Rules updated (v6)
 vagrant@vagrant:~$ sudo ufw allow https
 Rules updated
 Rules updated (v6)
+vagrant@vagrant:~$  
+
+```
+vagrant@vagrant:~$ sudo nano /etc/apt/sources.list.d/nginx.list
+vagrant@vagrant:~$ wget http://nginx.org/keys/nginx_signing.key
+--2022-04-05 08:28:47--  http://nginx.org/keys/nginx_signing.key
+Resolving nginx.org (nginx.org)... 3.125.197.172, 52.58.199.22, 2a05:d014:edb:5702::6, ...
+Connecting to nginx.org (nginx.org)|3.125.197.172|:80... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 1561 (1.5K) [application/octet-stream]
+Saving to: ‘nginx_signing.key’
+
+nginx_signing.key                                           100%[========================================================================================================================================>]   1.52K  --.-KB/s    in 0s
+
+2022-04-05 08:28:48 (76.7 MB/s) - ‘nginx_signing.key’ saved [1561/1561]
+
+vagrant@vagrant:~$ sudo apt-key add nginx_signing.key
+OK
 vagrant@vagrant:~$
+
+vagrant@vagrant:~$ sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+
+```
---
+++ Exam-01/Script.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import sys
+import datetime
+
+# Print to logfile
+old_stdout = sys.stdout
+log_file = open("nginx_cert.log", "w")
+sys.stdout = log_file
+print('******* ' + datetime.datetime.today().strftime("%d-%m-%Y %H.%M.%S") + ' *******\n')
+
+# Check the Environment
+cert_string = "vault write -format=json pki/issue/example-dot-com common_name=\"last.example.com\" ttl=\"720h\""
+
+if os.popen("echo $VAULT_ADDR").read():  # Test VAULT settings
+    cert_data = json.loads(os.popen(cert_string).read())  # Get new certs
+    with open("/etc/nginx/last.example.com.fullchain.pem", 'w') as cert_file:
+        cert_pem = cert_data['data']['certificate']+'\n'+cert_data['data']['issuing_ca']
+        cert_file.write(cert_pem)  # Save new certs to file
+    with open("/etc/nginx/last.example.com.key.pem", 'w') as key_file:
+        key_pem = cert_data['data']['private_key']
+        key_file.write(key_pem)  # Save new priv key
+    if os.popen("nginx -t").read().find('test is successful'):  # Test nginx config
+        os.popen("systemctl restart nginx")  # Restart nginx
+    else:
+        print("Configuration test failed!!!\n")
+else:
+    print("Environment variables NOT set!\n")
+
+# Close logfile
+sys.stdout = old_stdout
+log_file.close()
